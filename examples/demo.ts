dc9cf607ae890bdc94147583caa7d2bd65bec3ae
Kamila Cermakova <kamca.mar@gmail.com>
2025-12-15 09:58:46 +0100

    úprava dema

--- examples/demo.ts
+++ examples/demo.ts
@@ -1,12 +1,7 @@
 import { createAresClient } from '../src/ares';
 
 (async () => {
-  const client = createAresClient({
-    logger: (msg, level = 'info') => console.log(`[${level}] ${msg}`)
-  });
-
-  const ico = process.argv[2] ?? '27074358'; // volitelně: npm run demo -- 27074358
-  const info = await client.getAresInfo(ico);
-
-  console.log(JSON.stringify(info, null, 2));
+  const client = createAresClient();
+  const info = await client.getAresInfo('27074358');
+  console.log(info);
 })();
